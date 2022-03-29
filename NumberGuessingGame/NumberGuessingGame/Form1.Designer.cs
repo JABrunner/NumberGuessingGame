8af42e130fdb38ea9bcc30610a31194e2533d855
Jordan Brunner <89763188+Xiagax@users.noreply.github.com>
2022-03-29 16:07:03 -0500

    Add files via upload

---
+++ NumberGuessingGame/NumberGuessingGame/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace NumberGuessingGame
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblText = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblText
+            // 
+            this.lblText.AutoSize = true;
+            this.lblText.Font = new System.Drawing.Font("Microsoft YaHei", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblText.Location = new System.Drawing.Point(69, 58);
+            this.lblText.Name = "lblText";
+            this.lblText.Size = new System.Drawing.Size(37, 26);
+            this.lblText.TabIndex = 0;
+            this.lblText.Text = "lbl";
+            this.lblText.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(74, 215);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBox1.Size = new System.Drawing.Size(315, 50);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuess.Location = new System.Drawing.Point(160, 317);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(142, 46);
+            this.btnGuess.TabIndex = 2;
+            this.btnGuess.Text = "Guess";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(114, 413);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 28);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Score: ";
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft YaHei", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(203, 413);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(25, 28);
+            this.lblScore.TabIndex = 4;
+            this.lblScore.Text = "0";
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(452, 471);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblText);
+            this.Name = "main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Number Guessing Game";
+            this.Load += new System.EventHandler(this.main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblText;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblScore;
+    }
+}
+
---
+++ NumberGuessingGame/NumberGuessingGame/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NumberGuessingGame
+{
+    public partial class main : Form
+    {
+        Random random = new Random();
+        String guess = ""; 
+        
+         
+        public main()
+        {
+            InitializeComponent();
+            this.AcceptButton = btnGuess;
+            String guess = "";
+            
+           
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+            lblText.Text = "Guess a number \nbetween 1 and 100";
+            
+            
+        }
+        
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            guess = textBox1.Text;
+            checkWinner(guess);
+        }
+
+        public void checkWinner(String userGuess)
+        {
+            try { 
+            int numToGuess = random.Next(1, 100);
+            int guessedNum = int.Parse(guess);
+
+            if(guessedNum <1 || guessedNum > 100)
+             {
+                    MessageBox.Show("That's not a number between 1 and 100", "Error");
+                    lblText.Text = "Guess a number \nbetween 1 and 100";
+             }
+            if(numToGuess < guessedNum)
+            {
+                //Output message if the number guessed it too high
+                lblText.Text = "Your guess was Too High, \nThe Number was: " + numToGuess +"\n Play again?";
+                textBox1.Clear();
+
+            }
+
+            if(numToGuess > guessedNum)
+            {
+                //output message if too low
+                lblText.Text = "Your guess was Too low, \nThe Number was: " + numToGuess + "\n Play again?";
+                    textBox1.Clear();
+            }
+            if(numToGuess == guessedNum)
+            {
+
+                //output message box if guess is correct
+                MessageBox.Show("You Win", "A WINNER IS YOU");
+                    int currentScore;
+                    currentScore = 0;
+                    currentScore++;
+                        lblScore.Text = currentScore.ToString();
+                textBox1.Clear();
+            }
+            
+          
+           }
+           catch (Exception e)
+            {
+                MessageBox.Show("That's not a number between 1 and 100 you dingus", "ERROR!!!");
+                 textBox1.Clear();
+            }
+        }
+    }
+}
